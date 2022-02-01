5470255baac487e9aeecfd232385a1d9638b87b0
David O'Callaghan <dave@docallag.com>
2022-02-01 11:13:23 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,16 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Beyond Genesis!</title>
+<!--        <link rel="icon" href="/favicon.ico" />-->
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <Header title="29 AB 5F 49" />
         </p>
       </main>
 
-      <Footer />
+      <!--<Footer />-->
     </div>
   )
 }
